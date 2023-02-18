67fdf5d3a06b035db2f43b667f5f16c77431135c
LevanNamicheishvili <117360031+LevanNamicheishvili@users.noreply.github.com>
2023-02-19 03:13:18 +0400

    test version js
    refactoringi sachiroa uechveli XD

--- static/script/js/app.js
+++ static/script/js/app.js
@@ -1,7 +1,32 @@
-const hamburger = document.querySelector('.hamburger')
-const navLinks= document.querySelector('.navbar-links')
+const mainSection = document.getElementById('card-container');
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.navbar-links');
 
 hamburger.addEventListener('click', () => { 
-    hamburger.classList.toggle('active')
-    navLinks.classList.toggle('show')
-})
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('show');
+});
+
+try {
+  for (let i = 1; i <= 10; i++) {
+    const url = `https://fakestoreapi.com/products/${i}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+        <div class="card_img_frame" > 
+        <img src="${data.image}" alt="${data.title}" >
+        </div>
+          <h2>${data.title}</h2>
+          <p>Category: ${data.category}</p>
+          <p>Price: ${data.price}</p>
+        `;
+        mainSection.appendChild(card);
+      });
+  }
+} catch (error) {
+  console.log('An error occurred while fetching data: ', error);
+}
+
