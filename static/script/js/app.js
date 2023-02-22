652a2645a0b75f66227349322a20a768e38ebe9b
LevanNamicheishvili <117360031+LevanNamicheishvili@users.noreply.github.com>
2023-02-23 02:07:10 +0400

    bag fix
    remove loop

--- static/script/js/app.js
+++ static/script/js/app.js
@@ -1,3 +1,87 @@
+// const mainSection = document.getElementById('card-container');
+// const hamburger = document.querySelector('.hamburger');
+// const navLinks = document.querySelector('.navbar-links');
+// const list = document.getElementById('myList');
+// const items = list.getElementsByTagName('li');
+// const nextButton = document.getElementById('btn2');
+// const prevBtn = document.getElementById('btn1');
+// let pageNumber = 1;
+// let activeIndex = 0;
+
+// function closeHandler() {
+//   for (const item of items) {
+//     item.classList.remove('active');
+//   }
+// }
+
+// function setActive(index) {
+//   closeHandler();
+//   items[index].classList.add('active');
+// }
+
+// function loadProducts() {
+//   for (let i = 1; i <= 10; i++) {
+//     const url = `https://fakestoreapi.com/products/${pageNumber}`;
+//     fetch(url)
+//       .then(response => response.json())
+//       .then(data => {
+//         const card = document.createElement('div');
+//         card.classList.add('card');
+//         card.innerHTML = `
+//           <div class="card_img_frame">
+//             <img src="${data.image}" alt="${data.title}">
+//           </div>
+//           <h2>${data.title}</h2>
+//           <p>Category: ${data.category}</p>
+//           <p>Price: ${data.price}</p>
+//         `;
+//         mainSection.appendChild(card);
+//       })
+//       .catch(error => {
+//         console.log(`Error fetching product data: ${error}`);
+//       });
+//   }
+// }
+
+// function handleListClick(event) {
+//   if (event.target.tagName === 'LI') {
+//     closeHandler();
+//     event.target.classList.add('active');
+//     activeIndex = Array.from(items).indexOf(event.target);
+//     pageNumber = activeIndex + 1;
+//     mainSection.innerHTML = '';
+//     loadProducts();
+//   }
+// }
+
+// function handleNextClick() {
+//   activeIndex = (activeIndex + 1) % items.length;
+//   setActive(activeIndex);
+//   pageNumber++;
+//   mainSection.innerHTML = '';
+//   loadProducts();
+// }
+
+// function handlePrevClick() {
+//   activeIndex = (activeIndex - 1 + items.length) % items.length;
+//   setActive(activeIndex);
+//   pageNumber--;
+//   mainSection.innerHTML = '';
+//   loadProducts();
+// }
+
+// function handleHamburgerClick() { 
+//   hamburger.classList.toggle('active');
+//   navLinks.classList.toggle('show');
+// }
+
+// list.addEventListener('click', handleListClick);
+// nextButton.addEventListener('click', handleNextClick);
+// prevBtn.addEventListener('click', handlePrevClick);
+// hamburger.addEventListener('click', handleHamburgerClick);
+
+// setActive(activeIndex);
+// loadProducts();
 const mainSection = document.getElementById('card-container');
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.navbar-links');
@@ -8,42 +92,41 @@ const prevBtn = document.getElementById('btn1');
 let pageNumber = 1;
 let activeIndex = 0;
 
-function closeHandler() {
+const closeHandler = () => {
   for (const item of items) {
     item.classList.remove('active');
   }
-}
+};
 
-function setActive(index) {
+const setActive = (index) => {
   closeHandler();
   items[index].classList.add('active');
-}
+};
 
-function loadProducts() {
-  for (let i = 1; i <= 10; i++) {
-    const url = `https://fakestoreapi.com/products/${pageNumber}`;
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.innerHTML = `
-          <div class="card_img_frame">
-            <img src="${data.image}" alt="${data.title}">
-          </div>
-          <h2>${data.title}</h2>
-          <p>Category: ${data.category}</p>
-          <p>Price: ${data.price}</p>
-        `;
-        mainSection.appendChild(card);
-      })
-      .catch(error => {
-        console.log(`Error fetching product data: ${error}`);
-      });
-  }
-}
+const loadProducts = () => {
+  const url = `https://fakestoreapi.com/products/${pageNumber}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const card = document.createElement('div');
+      card.classList.add('card');
+      card.innerHTML = `
+        <div class="card_img_frame">
+          <img src="${data.image}" alt="${data.title}">
+        </div>
+        <h2>${data.title}</h2>
+        <p>Category: ${data.category}</p>
+        <p>Price: ${data.price}</p>
+      `;
+      mainSection.appendChild(card);
+    })
+    .catch((error) => {
+      console.log(`Error fetching product data: ${error}`);
+    });
 
-function handleListClick(event) {
+};
+
+const handleListClick = (event) => {
   if (event.target.tagName === 'LI') {
     closeHandler();
     event.target.classList.add('active');
@@ -52,28 +135,28 @@ function handleListClick(event) {
     mainSection.innerHTML = '';
     loadProducts();
   }
-}
+};
 
-function handleNextClick() {
+const handleNextClick = () => {
   activeIndex = (activeIndex + 1) % items.length;
   setActive(activeIndex);
   pageNumber++;
   mainSection.innerHTML = '';
   loadProducts();
-}
+};
 
-function handlePrevClick() {
+const handlePrevClick = () => {
   activeIndex = (activeIndex - 1 + items.length) % items.length;
   setActive(activeIndex);
   pageNumber--;
   mainSection.innerHTML = '';
   loadProducts();
-}
+};
 
-function handleHamburgerClick() { 
+const handleHamburgerClick = () => { 
   hamburger.classList.toggle('active');
   navLinks.classList.toggle('show');
-}
+};
 
 list.addEventListener('click', handleListClick);
 nextButton.addEventListener('click', handleNextClick);
